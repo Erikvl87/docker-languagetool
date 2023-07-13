7d25e38f304eca4733e051096f5c0c5a80eb2b7a
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2023-07-13 20:56:21 +0300

    Allow modifying `languagetool` logging
    Inspired by https://github.com/languagetool-org/languagetool/issues/8882.
    
    On top of the default template, a simple
    ```xml
    <logger name="org.languagetool.server.LanguageToolHttpHandler" level="WARNING" />
    ```
    should* be enough to silence the continuous spam.
    
    Additionally, for `start.sh`:
    * Fix formatting
    * Fix a _useless use of `cat`_
    * Move args to the `ARGS` array, for easier manipulation
    
    Signed-off-by: Stavros Ntentos <133706+stdedos@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -7,12 +7,27 @@ do
 done
 
 if [ "$config_injected" = true ] ; then
-    echo 'The following configuration is passed to LanguageTool:'
-	echo "$(cat config.properties)"
+  echo 'The following configuration is passed to LanguageTool:'
+  cat config.properties
 fi
 
 Xms=${Java_Xms:-256m}
 Xmx=${Java_Xmx:-512m}
 
+ARGS=(
+  "-Xms$Xms"
+  "-Xmx$Xmx"
+  -cp languagetool-server.jar
+  org.languagetool.server.HTTPServer
+  --port 8010
+  --public
+  --allow-origin '*'
+  --config config.properties
+)
+
+if [ -f /languagetool-logback.xml ] ; then
+  ARGS+=("-Dlogback.configurationFile=/languagetool-logback.xml")
+fi
+
 set -x
-java -Xms$Xms -Xmx$Xmx -cp languagetool-server.jar org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' --config config.properties
+java "${ARGS[@]}"
